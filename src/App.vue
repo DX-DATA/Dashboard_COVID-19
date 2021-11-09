7788f2b26b9755a4ede19510da7a4cf61bf33eb2
markyul <pig68111000@gmail.com>
2021-11-09 13:22:39 +0900

    create vue project

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <Main msg="Welcome to Your Vue.js App" />
+</template>
+
+<script>
+import Main from './components/Main.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Main,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>COVID-19 Dashboard</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Main',
+  props: {},
+};
+</script>
+
+<style></style>

eedffe5ff6f99d3dfc6899d9d634af4f7582afcf
markyul <pig68111000@gmail.com>
2021-11-16 14:23:58 +0900

    top navbar 구현

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -11,37 +11,44 @@
     </button>
     <div class="collapse navbar-collapse" id="sidebar">
       <div class="container-fluid">
-        <div class="row">
-          <!-- sidebar -->
-          <div class="col-lg-2 sidebar fixed-top">
-            <a
-              href="#"
-              class="
-                navbar-brand
-                text-white text-center
-                d-block
-                mx-auto
-                py-3
-                mb-4
-                bottom-border
-              "
-              >COVID-19</a
-            >
-            <ul class="navbar-nav flex-column mt-4">
-              <li class="nav-item">
-                <a href="#" class="nav-link text-white p-3 mb-2 current">
-                  <font-awesome-icon icon="home" />
-                  Dashboard
-                </a>
-                <a href="#" class="nav-link text-white p-3 mb-2 sidebar-link">
-                  <font-awesome-icon icon="wrench" />
-                  Settings
-                </a>
-              </li>
-            </ul>
+        <!-- top navber -->
+        <div class="ml-auto bg-dark fixed-top py-3 top-navbar">
+          <div class="row">
+            <div class="col-md-2"></div>
+            <div class="col-md-2">
+              <h4 class="text-light text-uppercase mb-0">Dashboard</h4>
+            </div>
+            <div class="col-md-5"></div>
+            <div class="col-md-3"></div>
           </div>
-          <!-- top navber -->
-          <div class="col-lg-9"></div>
+        </div>
+        <!-- sidebar -->
+        <div class="col-lg-2 sidebar fixed-top">
+          <a
+            href="#"
+            class="
+              navbar-brand
+              text-white text-center
+              d-block
+              mx-auto
+              py-3
+              mb-4
+              bottom-border
+            "
+            >COVID-19</a
+          >
+          <ul class="navbar-nav flex-column mt-4">
+            <li class="nav-item">
+              <a href="#" class="nav-link text-white p-3 mb-2 current">
+                <font-awesome-icon icon="home" />
+                Dashboard
+              </a>
+              <a href="#" class="nav-link text-white p-3 mb-2 sidebar-link">
+                <font-awesome-icon icon="wrench" />
+                Settings
+              </a>
+            </li>
+          </ul>
         </div>
       </div>
     </div>
@@ -58,11 +65,10 @@ export default {
 <style>
 .sidebar {
   height: 100vh;
-  background: linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.9));
+  background: linear-gradient(rgba(0, 0, 0, 1), rgba(0, 0, 0, 1));
   background-position: center;
   background-repeat: no-repeat;
   background-size: cover;
-  box-shadow: 5px 7px 25px #999;
 }
 .bottom-border {
   border-bottom: 2px solid #eee;
@@ -70,7 +76,6 @@ export default {
 .current {
   background-color: #f44336;
   border-radius: 7px;
-  box-shadow: 2px 5px 10px #111;
 }
 .current:hover {
   background-color: #f66436;

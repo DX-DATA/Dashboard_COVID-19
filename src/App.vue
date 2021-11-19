d71d0946a05ff8dba962ada371bbd48beab3481c
markyul <pig68111000@gmail.com>
2021-11-19 14:22:54 +0900

    vue-chart-js로 chart 연습

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <Main msg="Welcome to Your Vue.js App" />
+  <Main />
 </template>
 
 <script>
@@ -14,12 +14,12 @@ export default {
 </script>
 
 <style>
-#app {
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+} */
 </style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,86 +1,96 @@
 <template>
-  <!-- navbar -->
-  <nav class="navbar navbar-expand-md navber-light">
-    <button
-      class="navbar-toggler ml-auto mb-2 bg-light"
-      type="button"
-      data-toggle="collapse"
-      data-target="#sidebar"
-    >
-      <span class="navber-toggle-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="sidebar">
-      <div class="container-fluid">
-        <!-- top navber -->
-        <div class="ml-auto bg-dark fixed-top py-3 top-navbar">
-          <div class="row">
-            <div class="col-md-2"></div>
-            <div class="col-md-2">
-              <h4 class="text-light text-uppercase mb-0">Dashboard</h4>
-            </div>
-            <div class="col-md-5"></div>
-            <div class="col-md-3"></div>
-          </div>
-        </div>
-        <!-- sidebar -->
-        <div class="col-lg-2 sidebar fixed-top">
-          <a
-            href="#"
-            class="
-              navbar-brand
-              text-white text-center
-              d-block
-              mx-auto
-              py-3
-              mb-4
-              bottom-border
-            "
-            >COVID-19</a
-          >
-          <ul class="navbar-nav flex-column mt-4">
-            <li class="nav-item">
-              <a href="#" class="nav-link text-white p-3 mb-2 current">
-                <font-awesome-icon icon="home" />
-                Dashboard
-              </a>
-              <a href="#" class="nav-link text-white p-3 mb-2 sidebar-link">
-                <font-awesome-icon icon="wrench" />
-                Settings
-              </a>
-            </li>
-          </ul>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNavAltMarkup"
+        aria-controls="navbarNavAltMarkup"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+          <a class="nav-link" href="#">Features</a>
+          <a class="nav-link" href="#">Pricing</a>
         </div>
       </div>
     </div>
   </nav>
+
+  <section>
+    <DoughnutChart :chartData="doughnutTestData" :options="doughnutOption" />
+    <LineChart :chartData="lineTestData" />
+  </section>
 </template>
 
 <script>
+import { ref } from 'vue';
+import { DoughnutChart, LineChart } from 'vue-chart-3';
+import { Chart, registerables } from 'chart.js';
+
+Chart.register(...registerables);
+
 export default {
   name: 'Main',
+  components: {
+    DoughnutChart,
+    LineChart,
+  },
   props: {},
+  setup() {
+    const data = ref([30, 40, 60, 70, 5]);
+    // Doughnut용
+    const doughnutTestData = {
+      labels: ['Paris', 'Nîmes', 'Toulon', 'Perpignan', 'Autre'],
+      datasets: [
+        {
+          data: data.value,
+          backgroundColor: [
+            '#77CEFF',
+            '#0079AF',
+            '#123E6B',
+            '#97B0C4',
+            '#A5C8ED',
+          ],
+        },
+      ],
+    };
+    const doughnutOption = ref({
+      responsive: true,
+      plugins: {
+        legend: {
+          position: 'top',
+        },
+        title: {
+          display: true,
+          text: 'Chart.js Doughnut Chart',
+        },
+      },
+    });
+
+    const lineTestData = {
+      labels: ['Paris', 'Nîmes', 'Toulon', 'Perpignan', 'Autre'],
+      datasets: [
+        {
+          label: 'Contry',
+          data: data.value,
+          pointBackgroundColor: 'white',
+          borderWidth: 1,
+          borderColor: '#77CEFF',
+          pointBorderColor: 'blue',
+        },
+      ],
+    };
+
+    return { doughnutTestData, doughnutOption, lineTestData };
+  },
 };
 </script>
 
-<style>
-.sidebar {
-  height: 100vh;
-  background: linear-gradient(rgba(0, 0, 0, 1), rgba(0, 0, 0, 1));
-  background-position: center;
-  background-repeat: no-repeat;
-  background-size: cover;
-}
-.bottom-border {
-  border-bottom: 2px solid #eee;
-}
-.current {
-  background-color: #f44336;
-  border-radius: 7px;
-}
-.current:hover {
-  background-color: #f66436;
-  border-radius: 7px;
-  box-shadow: 2px 5px 10px #111;
-  transform: translateY(-1px);
-}
-</style>
+<style></style>

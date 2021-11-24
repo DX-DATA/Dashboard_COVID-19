8b935c1ac6c40e2efa4ad0d6ea85e21aaf59a593
markyul <pig68111000@gmail.com>
2021-11-24 14:13:08 +0900

    백신 관련 정보 chart로 구현

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -22,17 +22,21 @@
       </div>
     </div>
   </nav>
-
+  <button v-on:click="test">test</button>
   <section>
     <DoughnutChart :chartData="doughnutTestData" :options="doughnutOption" />
-    <LineChart :chartData="lineTestData" />
+    <LineChart :chartData="vaccineLineData" :options="lineOption" />
   </section>
 </template>
 
 <script>
-import { ref } from 'vue';
+import { computed, onMounted, ref } from 'vue';
+import axios from 'axios';
+
 import { DoughnutChart, LineChart } from 'vue-chart-3';
 import { Chart, registerables } from 'chart.js';
+
+import { shuffle } from 'lodash';
 
 Chart.register(...registerables);
 
@@ -44,23 +48,72 @@ export default {
   },
   props: {},
   setup() {
-    const data = ref([30, 40, 60, 70, 5]);
+    let temp = ref([1, 2, 3, 4, 5]);
+    const doughnutRef = ref();
+    const lineRef = ref();
+
+    const vacLabel = ref([]);
+    const firstData = ref([]);
+    const secondData = ref([]);
+    const thirdData = ref([]);
+    const vaccineTotalData = ref([]);
+
+    function test() {
+      temp.value = shuffle(temp.value);
+    }
+
+    async function getVaccine() {
+      const tempTotalData = [];
+      const tempFirstData = [];
+      const tempSecondData = [];
+      const tempThirdData = [];
+
+      await axios
+        .get('http://192.168.0.21:3000/api/vaccine?sido=전국')
+        .then((res) => {
+          // 차수 별 총 접종 수를 최신 일자로 가져오기
+          let newInfo = res.data[res.data.length - 1];
+          tempTotalData.push(
+            newInfo.total_first,
+            newInfo.total_second,
+            newInfo.total_third
+          );
+
+          // 날짜 순으로 차수 별 접종 data 가져오기
+          res.data.map((v) => {
+            const date = new Date(v.date);
+            console.log(date);
+
+            vacLabel.value.push(
+              date.getMonth() + 1 + '월' + date.getDate() + '일'
+            );
+            tempFirstData.push(v.first);
+            tempSecondData.push(v.second);
+            tempThirdData.push(v.third);
+          });
+        })
+        .catch((e) => console.log(e));
+
+      firstData.value = tempFirstData;
+      secondData.value = tempSecondData;
+      thirdData.value = tempThirdData;
+      vaccineTotalData.value = tempTotalData;
+    }
+
+    onMounted(async () => {
+      await getVaccine();
+    });
+
     // Doughnut용
-    const doughnutTestData = {
-      labels: ['Paris', 'Nîmes', 'Toulon', 'Perpignan', 'Autre'],
+    const doughnutTestData = computed(() => ({
+      labels: ['1차 백신', '2차 백신', '3차 백신'],
       datasets: [
         {
-          data: data.value,
-          backgroundColor: [
-            '#77CEFF',
-            '#0079AF',
-            '#123E6B',
-            '#97B0C4',
-            '#A5C8ED',
-          ],
+          data: vaccineTotalData.value,
+          backgroundColor: ['#77CEFF', '#0079AF', '#123E6B'],
         },
       ],
-    };
+    }));
     const doughnutOption = ref({
       responsive: true,
       plugins: {
@@ -69,26 +122,64 @@ export default {
         },
         title: {
           display: true,
-          text: 'Chart.js Doughnut Chart',
+          text: '백신 차수별 총 접종 그래프',
         },
       },
     });
 
-    const lineTestData = {
-      labels: ['Paris', 'Nîmes', 'Toulon', 'Perpignan', 'Autre'],
+    const vaccineLineData = computed(() => ({
+      labels: vacLabel.value,
       datasets: [
         {
-          label: 'Contry',
-          data: data.value,
+          label: '1차 백신',
+          data: firstData.value,
           pointBackgroundColor: 'white',
           borderWidth: 1,
           borderColor: '#77CEFF',
-          pointBorderColor: 'blue',
+          pointBorderColor: 'black',
+        },
+        {
+          label: '2차 백신',
+          data: secondData.value,
+          pointBackgroundColor: 'white',
+          borderWidth: 1,
+          borderColor: 'red',
+          pointBorderColor: 'black',
+        },
+        {
+          label: '3차 백신',
+          data: thirdData.value,
+          pointBackgroundColor: 'white',
+          borderWidth: 1,
+          borderColor: '#333333',
+          pointBorderColor: 'black',
         },
       ],
-    };
+    }));
+    const lineOption = ref({
+      responsive: true,
+      plugins: {
+        legend: {
+          position: 'top',
+        },
+        title: {
+          display: true,
+          text: '차수별 접종 추이',
+        },
+      },
+    });
 
-    return { doughnutTestData, doughnutOption, lineTestData };
+    return {
+      vaccineLineData,
+      temp,
+      doughnutRef,
+      lineRef,
+      doughnutTestData,
+      doughnutOption,
+      vaccineTotalData,
+      lineOption,
+      test,
+    };
   },
 };
 </script>

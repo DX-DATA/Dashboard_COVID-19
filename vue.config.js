9f1cbbf2ed55b78a69348cba1a81572b2619755e
markyul <pig68111000@gmail.com>
2021-12-01 13:45:09 +0900

    api 주소 변경

--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,18 @@
 module.exports = {
-	devServer: {
-        proxy: { 
-            '/api': { 
-                target: 'http://localhost:3333/', 
-                changeOrigin: true, 
-                pathRewrite:{ "^/api" : '' } 
-            },
-            '/socket': { 
-                target: 'http://127.0.0.1:3333/', 
-                changeOrigin: true, 
-                pathRewrite:{ "^/socket" : '' },
-                ws: false
-            } 
-        },
-		headers: { "Access-Control-Allow-Origin": "*" }
-	}
-}
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://192.168.0.21:3333/',
+        changeOrigin: true,
+        pathRewrite: { '^/api': '' },
+      },
+      '/socket': {
+        target: 'http://192.168.0.21:3333/',
+        changeOrigin: true,
+        pathRewrite: { '^/socket': '' },
+        ws: false,
+      },
+    },
+    headers: { 'Access-Control-Allow-Origin': '*' },
+  },
+};

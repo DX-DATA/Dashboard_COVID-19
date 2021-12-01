74004afa8be852b4b52990b9855bf935909a83b9
kwonjuhyeon <asshole96@naver.com>
2021-12-01 13:19:11 +0900

    Elecar, router 수정, 레이아웃 변경
    .

--- src/main.js
+++ src/main.js
@@ -3,8 +3,9 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import router from './router';
+import elecarRouter from './router/index'
 import App from './App.vue';
 
 library.add(fas);
 
-createApp(App).component('font-awesome-icon', FontAwesomeIcon).use(router).mount('#app');
+createApp(App).component('font-awesome-icon', FontAwesomeIcon).use(router, elecarRouter).mount('#app');

9b3d0e23dab89e5ffb9300612e25e406812275c1
markyul <pig68111000@gmail.com>
2021-12-01 14:59:14 +0900

    navbar 활성화된 탭 표시

--- src/components/layout/Nav.vue
+++ src/components/layout/Nav.vue
@@ -1,62 +1,105 @@
 <template>
-<div class="nav_component d-flex flex-column ">
+  <div class="nav_component d-flex flex-column">
     <div class="nav-title">
-        <h1>DXDATA DashBoard</h1>
+      <h1>DXDATA DashBoard</h1>
     </div>
-        <ul class="nav nav-pills flex-column mb-auto">
-            <li>
-              <router-link to="/" class="nav_link active">
-                <i class="fas fa-home"></i> Home
-              </router-link>
-            </li>
-            <li><router-link to="/vaccine" class="nav_link text-white"><i class="fas fa-syringe"></i> 백신</router-link></li>
-            <li><router-link to="/elecar" class="nav_link text-white"><i class="fas fa-level-up-alt"></i> 고소차</router-link></li>
-            <li><router-link to="/crane" class="nav_link text-white"><i class="fas fa-cubes"></i> 크레인</router-link></li>
-            <li><a href="#" v-on:click="logout" class="nav_link text-white">로그아웃</a></li>
+    <ul class="nav nav-pills flex-column mb-auto">
+      <li>
+        <router-link to="/" class="nav_link text-white">
+          <i class="fas fa-home"></i> Home
+        </router-link>
+      </li>
+      <li>
+        <router-link to="/vaccine" class="nav_link text-white"
+          ><i class="fas fa-syringe"></i> 백신</router-link
+        >
+      </li>
+      <li>
+        <router-link to="/elecar" class="nav_link text-white"
+          ><i class="fas fa-level-up-alt"></i> 고소차</router-link
+        >
+      </li>
+      <li>
+        <router-link to="/crane" class="nav_link text-white"
+          ><i class="fas fa-cubes"></i> 크레인</router-link
+        >
+      </li>
+      <li>
+        <a href="#" v-on:click="logout" class="nav_link text-white">로그아웃</a>
+      </li>
     </ul>
     <div class="dropdown">
-      <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
-        <img src="https://github.com/mdo.png" alt="" width="32" height="32" class="rounded-circle me-2">
+      <a
+        href="#"
+        class="
+          d-flex
+          align-items-center
+          text-white text-decoration-none
+          dropdown-toggle
+        "
+        id="dropdownUser1"
+        data-bs-toggle="dropdown"
+        aria-expanded="false"
+      >
+        <img
+          src="https://github.com/mdo.png"
+          alt=""
+          width="32"
+          height="32"
+          class="rounded-circle me-2"
+        />
         <strong>Test</strong>
       </a>
-      <ul class="dropdown-menu dropdown-menu-dark text-small shadow" aria-labelledby="dropdownUser1" style="">
+      <ul
+        class="dropdown-menu dropdown-menu-dark text-small shadow"
+        aria-labelledby="dropdownUser1"
+        style=""
+      >
         <li><a class="dropdown-item" href="#">New project...</a></li>
         <li><a class="dropdown-item" href="#">Settings</a></li>
         <li><a class="dropdown-item" href="#">Profile</a></li>
-        <li><hr class="dropdown-divider"></li>
+        <li><hr class="dropdown-divider" /></li>
         <li><a class="dropdown-item" v-on:click="logout">Sign out</a></li>
       </ul>
     </div>
-</div>
+  </div>
 </template>
 
 <script>
 export default {
   methods: {
-    logout : function() {
-        document.cookie = "auth=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-        location.reload();
+    logout: function () {
+      document.cookie = 'auth=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
+      location.reload();
+    },
+  },
+  watch: {
+    $route: {
+      handler(val) {
+        console.log(val.path);
       },
-  }
-}
+      immediate: true,
+    },
+  },
+};
 </script>
 <style scoped>
-.nav-title > h1 {  
+.nav-title > h1 {
   font-size: 18px;
   font-weight: lighter;
 }
 
 .nav_component {
-    background: none;
-    height: 100vh;
+  background: none;
+  height: 100vh;
 }
-.nav_component > ul{
+.nav_component > ul {
   list-style: none;
-  padding-left : 0px;
+  padding-left: 0px;
 }
 
-.nav_component >ul > li {
-  padding : 10px 0px 10px 0px;
+.nav_component > ul > li {
+  padding: 10px 0px 10px 0px;
 }
 
 .nav_component > ul > li > a {
@@ -70,19 +113,17 @@ export default {
 .nav_link {
   display: block;
   padding: 0.5rem 1rem;
-  color: #0d6efd;
   text-decoration: none;
-  transition: color .15s ease-in-out,background-color .15s ease-in-out,border-color .15s ease-in-out;
-}
-
-.active {
-  color: #fff;
-  background-color: #0d6efd!important;
+  transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out,
+    border-color 0.15s ease-in-out;
 }
 
 .nav_component > ul > li > a:hover {
-  color:#cccccc
+  background-color: #0d6dfd !important;
 }
 
-
+.nav a.router-link-active {
+  color: #fff;
+  background-color: #0d6efd !important;
+}
 </style>
